9a213029da9b4d3de529cf1dcfcd452d36029ad1
Almir Sarajčić <almir.sarajcic@icloud.com>
2017-08-17 14:39:26 +0200

    Initial commit

---
+++ lib/crypto_hustler.ex
@@ -0,0 +1,18 @@
+defmodule CryptoHustler do
+  @moduledoc """
+  Documentation for CryptoHustler.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CryptoHustler.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/crypto_hustler_test.exs
@@ -0,0 +1,8 @@
+defmodule CryptoHustlerTest do
+  use ExUnit.Case
+  doctest CryptoHustler
+
+  test "greets the world" do
+    assert CryptoHustler.hello() == :world
+  end
+end
