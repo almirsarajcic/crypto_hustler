c7aaa834dd76980e2752113a02dfd88c3c1f9253
Almir Sarajčić <almir.sarajcic@icloud.com>
2017-10-16 19:47:06 +0200

    Cover /account/getorder

--- lib/bittrex/order.ex
+++ lib/bittrex/order.ex
@@ -1,5 +1,5 @@
 defmodule Bittrex.Order do
-  defstruct [:uuid, :quantity, :rate, :market, :order_type, :quantity_remaining, :limit, :commission_paid, :price_per_unit, :opened_at, :closed_at, :cancel_initiated, :immediate_or_cancel, :conditional, :condition, :condition_target]
+  defstruct [:uuid, :account_id, :market,  :order_type, :quantity, :quantity_remaining, :limit, :reserved, :reserve_remaining, :commision, :commision_reserved, :commision_reserve_remaining, :commission_paid, :rate, :price_per_unit, :opened_at, :closed_at, :open, :sentinel, :cancel_initiated, :immediate_or_cancel, :conditional, :condition, :condition_target]
 
   def new(result) do
     %__MODULE__{
---
+++ lib/bittrex/service/account/get_order.ex
@@ -0,0 +1,47 @@
+defmodule Bittrex.Service.Account.GetOrder do
+  use Bittrex.Service
+
+  alias Bittrex.{Market, Order}
+
+  def call(%Order{uuid: uuid}) do
+    Request.new("/account/getorder", %{uuid: uuid})
+    |> Client.send()
+    |> format_response()
+  end
+
+  defp format_response({:ok, result}) do
+    response = parse_order(result)
+    {:ok, response}
+  end
+  defp format_response({:error, reason}), do: {:error, reason}
+
+  defp parse_order(result) do
+    %Order{
+      uuid: result["OrderUuid"],
+      account_id: result["AccountId"],
+      market: %Market{
+        name: result["Exchange"],
+      },
+      order_type: result["Type"],
+      quantity: result["Quantity"],
+      quantity_remaining: result["QuantityRemaining"],
+      limit: result["Limit"],
+      reserved: result["Reserved"],
+      reserve_remaining: result["ReserveRemaining"],
+      commision_reserved: result["CommissionReserved"],
+      commision_reserve_remaining: result["CommissionReserveRemaining"],
+      commission_paid: result["CommissionPaid"],
+      rate: result["Price"],
+      price_per_unit: result["PricePerUnit"],
+      opened_at: Bittrex.parse_datetime(result["Opened"]),
+      closed_at: Bittrex.parse_datetime(result["Closed"]),
+      open: result["IsOpen"],
+      sentinel: result["Sentinel"],
+      cancel_initiated: result["CancelInitiated"],
+      immediate_or_cancel: result["ImmediateOrCancel"],
+      conditional: result["IsConditional"],
+      condition: result["Condition"],
+      condition_target: result["ConditionTarget"],
+    }
+  end
+end
