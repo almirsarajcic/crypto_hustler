8993a2b87a9d8411a5503b5285b30a84f5cc5b05
Almir Sarajčić <almir.sarajcic@icloud.com>
2017-12-10 01:22:58 +0100

    Buy coins

---
+++ lib/crypto_hustler/bot.ex
@@ -0,0 +1,29 @@
+defmodule CryptoHustler.Bot do
+  alias Bittrex.Service.Account.GetBalances
+  alias Bittrex.Service.Market.BuyLimit
+  alias Bittrex.Service.Public.{GetCurrencies, GetMarkets, GetMarketSummaries}
+  alias CryptoHustler.{BtcBalanceCalculator, DataCombiner, MarketFilter, OrderPreparator}
+
+  @minimum_trade 0.00050000
+
+  def hustle do
+    {:ok, balances} = GetBalances.call()
+    {:ok, market_summaries} = GetMarketSummaries.call()
+
+    btc_balance = BtcBalanceCalculator.calculate(balances, market_summaries)
+
+    if btc_balance.available >= @minimum_trade do
+      {:ok, currencies} = GetCurrencies.call()
+      {:ok, markets} = GetMarkets.call()
+
+      currencies
+      |> DataCombiner.combine_currencies_with_markets(markets)
+      |> DataCombiner.combine_markets_with_market_summaries(market_summaries)
+      |> MarketFilter.filter(balances)
+      |> OrderPreparator.prepare_buy_orders(btc_balance.available)
+      |> Enum.each(&buy/1)
+    end
+  end
+
+  defp buy({market, order}), do: BuyLimit.call(market, order)
+end
