c668dafbf77ca5b564c4bcf5b54d2c2117378763
Almir Sarajčić <almir.sarajcic@icloud.com>
2018-01-09 16:17:23 +0100

    Add Coinmarketcap HttpClient

--- config/prod.exs
+++ config/prod.exs
@@ -5,6 +5,7 @@ config :crypto_hustler, :bittrex,
   base_url: "https://bittrex.com/api/v1.1",
   api_key: "${BITTREX_API_KEY}",
   api_secret: "${BITTREX_API_SECRET}"
+
 config :crypto_hustler, :bot,
   base_currency: "${BOT_BASE_CURRENCY}",
   number_of_coins: "${BOT_NUMBER_OF_COINS}",
