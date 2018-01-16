235bd70bf2e19f41a6908cc1a266acbbd46c23b8
Almir Sarajčić <almir.sarajcic@icloud.com>
2018-01-16 11:05:58 +0100

    Refactor ticker parsing

---
+++ lib/coinmarketcap/parser/ticker_parser.ex
@@ -0,0 +1,50 @@
+defmodule Coinmarketcap.Parser.TickerParser do
+  alias Coinmarketcap.Data.Ticker
+
+  def call(item, fiat_currency) do
+    converted = if fiat_currency do
+      lowercase = String.downcase(fiat_currency)
+
+      %{
+        currency: fiat_currency,
+        price: extract_float(item["price_" <> lowercase]),
+        market_cap: extract_float(item["market_cap_" <> lowercase]),
+        volume: extract_float(item["24h_volume_" <> lowercase]),
+      }
+    end
+
+    ticker = %Ticker{
+      id: item["id"],
+      name: item["name"],
+      symbol: item["symbol"],
+      rank: String.to_integer(item["rank"]),
+      usd_price: extract_float(item["price_usd"]),
+      btc_price: extract_float(item["price_btc"]),
+      volume: extract_float(item["24h_volume_usd"]),
+      market_cap: extract_float(item["market_cap_usd"]),
+      available_supply: extract_float(item["available_supply"]),
+      total_supply: extract_float(item["total_supply"]),
+      max_supply: extract_float(item["max_supply"]),
+      percentage_change: %{
+        hour: percentage_change(item, "percent_change_1h"),
+        day: percentage_change(item, "percent_change_24h"),
+        week: percentage_change(item, "percent_change_7d"),
+      },
+      updated_at: NaiveDateTime.add(~N[1970-01-01 00:00:00], String.to_integer(item["last_updated"])),
+      converted: converted
+    }
+  end
+
+  defp percentage_change(item, field) do
+    Float.round(extract_float(item[field]) / 100, 4)
+  end
+
+  defp extract_float(nil), do: nil
+  defp extract_float(string) do
+    unless Regex.match?(~r/\./, string) do
+      string = string <> ".0"
+    end
+
+    String.to_float(string)
+  end
+end
--- lib/coinmarketcap/service/get_tickers.ex
+++ lib/coinmarketcap/service/get_tickers.ex
@@ -1,5 +1,5 @@
 defmodule Coinmarketcap.Service.GetTickers do
-  alias Coinmarketcap.Data.Ticker
+  alias Coinmarketcap.Parser.TickerParser
   alias Coinmarketcap.{Client, Request, Response}
 
   def call(params \\ %{}) do
@@ -17,51 +17,7 @@ defmodule Coinmarketcap.Service.GetTickers do
   defp parse_tickers(_, _, tickers \\ [])
   defp parse_tickers([], _, tickers), do: tickers
   defp parse_tickers([head|tail], fiat_currency, tickers) do
-    converted = if fiat_currency do
-      lowercase = String.downcase(fiat_currency)
-
-      %{
-        currency: fiat_currency,
-        price: extract_float(head["price_" <> lowercase]),
-        market_cap: extract_float(head["market_cap_" <> lowercase]),
-        volume: extract_float(head["24h_volume_" <> lowercase]),
-      }
-    end
-
-    ticker = %Ticker{
-      id: head["id"],
-      name: head["name"],
-      symbol: head["symbol"],
-      rank: String.to_integer(head["rank"]),
-      usd_price: extract_float(head["price_usd"]),
-      btc_price: extract_float(head["price_btc"]),
-      volume: extract_float(head["24h_volume_usd"]),
-      market_cap: extract_float(head["market_cap_usd"]),
-      available_supply: extract_float(head["available_supply"]),
-      total_supply: extract_float(head["total_supply"]),
-      max_supply: extract_float(head["max_supply"]),
-      percentage_change: %{
-        hour: percentage_change(head, "percent_change_1h"),
-        day: percentage_change(head, "percent_change_24h"),
-        week: percentage_change(head, "percent_change_7d"),
-      },
-      updated_at: NaiveDateTime.add(~N[1970-01-01 00:00:00], String.to_integer(head["last_updated"])),
-      converted: converted
-    }
-
+    ticker = TickerParser.call(head, fiat_currency)
     parse_tickers(tail, fiat_currency, [ticker|tickers])
-  end
-
-  defp percentage_change(head, field) do
-    Float.round(extract_float(head[field]) / 100, 4)
-  end
-
-  defp extract_float(nil), do: nil
-  defp extract_float(string) do
-    unless Regex.match?(~r/\./, string) do
-      string = string <> ".0"
-    end
-
-    String.to_float(string)
   end
 end
